74606fb8054c9792def89aef06baa3aff906324f
turri21 <154733583+turri21@users.noreply.github.com>
2025-07-07 17:23:10 +0300

    Add files via upload

--- TurboGrafx16.sv
+++ TurboGrafx16.sv
@@ -57,6 +57,8 @@ module emu
 	input  [11:0] HDMI_WIDTH,
 	input  [11:0] HDMI_HEIGHT,
 	output        HDMI_FREEZE,
+	output        HDMI_BLACKOUT,
+	output        HDMI_BOB_DEINT,
 
 `ifdef MISTER_FB
 	// Use framebuffer in DDRAM
@@ -196,6 +198,8 @@ assign BUTTONS   = osd_btn;
 assign VGA_SCALER= 0;
 assign VGA_DISABLE = 0;
 assign HDMI_FREEZE = 0;
+assign HDMI_BLACKOUT = 0;
+assign HDMI_BOB_DEINT = 0;
 
 wire [1:0] ar       = status[25:24];
 wire       vcrop_en = status[32];
